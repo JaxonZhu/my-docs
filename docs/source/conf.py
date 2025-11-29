4b7cb483c32a49469abf4e75f3f20fc02884a52f
JinbiaoZhu <17836910686@163.com>
2025-11-30 00:15:06 +0800

    2025.11.30 / aw-win / modify myst_parser config to docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables'] 
+extensions = ['recommonmark','sphinx_markdown_tables', "myst_parser"] 
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
